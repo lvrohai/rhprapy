d79aa64949cf0b3f496074ad3adfdb1a3e580529
Diogenes <lvrohai@163.com>
2019-07-24 22:40:27 +0800

    修改了个别方法

--- days100/days1_15/11/untitled/file_operate_w.py
+++ days100/days1_15/11/untitled/file_operate_w.py
@@ -8,7 +8,27 @@ def is_prime(n):
             return False
     return True if n != 1 else False
 
-
+def main():
+    filenames = ('a.txt','b.txt','c.txt')
+    fs_list = []
+    try:
+        for filename in filenames:
+            fs_list.append(open(filename,'w',encoding='utf-8'))
+        for number in range(2, 10000):
+            if is_prime(number):
+                if number<100:
+                    fs_list[0].write(str(number) + '\n')
+                elif number < 1000:
+                    fs_list[1].write(str(number) + '\n')
+                else:
+                    fs_list[2].write(str(number) + '\n')
+    except IOError as ex:
+        print(ex)
+        print("io错误")
+    for fs in fs_list:
+        fs.close()
+    print("结束")
 
 if __name__ == '__main__':
     print(is_prime(4))
+    main()
