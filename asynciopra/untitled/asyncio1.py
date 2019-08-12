005e3cbb6e72779e222355b661d12389c8537fcd
Diogenes <lvrohai@163.com>
2019-08-12 19:15:28 +0800

    asyncio

--- asynciopra/untitled/asyncio1.py
+++ asynciopra/untitled/asyncio1.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # asyncio1.py
 
+import asyncio
 import time
 
 def hello():
@@ -12,5 +13,18 @@ def run():
         hello()
         print('Hello World:%s' % time.time())
 
+
+async def add(x=1, y=2):
+    print("Add {} + {} ...".format(x,y))
+    await asyncio.sleep(2)
+    return x + y
+
+s = time.time()
+loop = asyncio.get_event_loop()
+tasks = [add(x,y) for x,y in zip(range(1,10),range(11,20))]
+# loop.run_until_complete(add())
+loop.run_until_complete(asyncio.wait(tasks))
+print('cost:', time.time()-s)
+
 if __name__ == '__main__':
-    run()
+   pass
